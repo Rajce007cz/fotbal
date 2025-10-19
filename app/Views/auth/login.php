95c74e3881b1ca87afc813761e35359fbd77c261
Rajce007cz <krajca_michael@oauh.cz>
2025-10-20 00:25:14 +0200

    Upravený upload obrázků

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -3,7 +3,7 @@ $this->extend("layout/layout");
 $this->section("content"); ?>
 <div class="container my-5">
     <h2>Přihlášení do administrace</h2>
-    <form action="/krajca/fotbal/login" method="post">
+    <form action="<?= base_url('/login') ?>" method="post">
         <?= csrf_field() ?>
         <button type="submit" class="btn btn-primary">Přihlásit se</button>
     </form>
